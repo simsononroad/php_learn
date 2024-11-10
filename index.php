05a657f2c161d1f82f4cbe4cb051a4bb3d25c89b
simsononroad <d-github@kopaszhegy.hu>
2024-11-10 11:40:47 +0100

    switchs

--- index.php
+++ index.php
@@ -12,6 +12,26 @@
 
 
 <?php
-    
+    $grade = 5;
+    $date = date("1");
+    switch($grade){
+        case 5:
+            echo"Ügyes vagy!";
+            break;
+        case 4:
+            echo"Egész jó vagy!";
+            break;
+        case 3:
+            echo"jó vagy!";
+            break;
+        case 2:
+            echo"Megcsináltad!";
+            break;
+        case 1:
+            echo"Ez most nem ment!";
+            break;
+        default:
+            echo"{$grade} Nem egy létező jegy";
+    }
 ?>
 
---
+++ logical_operators.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is a php-file</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+<?php
+    $temp = 20;
+    if($temp >= 0 && $temp <= 25){
+        echo"The weather is good";
+    }
+    else{
+        echo"The weather is bad";
+    }
+?>
+
